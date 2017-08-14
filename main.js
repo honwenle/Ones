579ed7ba42d700ff735e8378038244d3699051e1
洪文乐 <noclip@foxmail.com>
2017-08-14 23:33:02 +0800

    动画

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ var back = document.getElementById('back');
 var can = document.getElementById('can');
 var btx = back.getContext('2d');
 var ctx = can.getContext('2d');
+var timer;
+var A = {},
+    B = {};
+var blockList = [];
 
 var lv = 2;
 
@@ -19,31 +23,49 @@ function drawBack() {
     btx.fillStyle = '#0f92bb';
     btx.fillRect(0, 0, window.screen.availWidth, window.screen.availHeight - 5 * SIZE);
 }
-function moveTo(y) {
+function moveTo(obj, toY) {
+    obj.y = window.screen.availHeight - obj.row * SIZE;
     cancelAnimationFrame(timer);
     timer = requestAnimationFrame(function fn() {
-        if (parseInt(myDiv.style.width) < 500) {
-            myDiv.style.width = parseInt(myDiv.style.width) + 5 + 'px';
-            myDiv.innerHTML = parseInt(myDiv.style.width) / 5 + '%';
+        if (obj.y < window.screen.availHeight - toY * SIZE) {
+            ctx.clearRect(obj.col * SIZE, 0, window.screen.availWidth, window.screen.availHeight);
+            obj.y += 25;
+            drawBlock(obj.col * SIZE, obj.y, obj.n);
             timer = requestAnimationFrame(fn);
         } else {
             cancelAnimationFrame(timer);
         }
     });
 }
+function drawBlockXY(obj) {
+    var x = obj.col * SIZE;
+    var y = window.screen.availHeight - obj.row * SIZE;
+    drawBlock(x, y, obj.n);
+}
+function setBlock(obj, x, y, n) {
+    obj.col = x;
+    obj.row = y;
+    obj.n = n;
+    blockList[getID(x, y)] = obj;
+}
+function getID(x, y) {
+    return x + y * 5;
+}
 function drawBlock(x, y, n) {
     ctx.fillStyle = colorArr[n];
-    ctx.fillRect(x * SIZE, window.screen.availHeight - y * SIZE - SIZE, SIZE, SIZE);
+    ctx.fillRect(x, y - SIZE, SIZE, SIZE);
     ctx.fill();
     ctx.fillStyle = '#555';
     ctx.font = SIZE - 10 + 'px 微软雅黑';
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
-    ctx.fillText(n, x * SIZE + SIZE / 2, window.screen.availHeight - y * SIZE - SIZE / 2);
+    ctx.fillText(n, x + SIZE / 2, y - SIZE / 2);
 }
 function newBlock() {
-    drawBlock(1, 5, ~~(Math.random() * lv) + 1);
-    drawBlock(2, 5, ~~(Math.random() * lv) + 1);
+    setBlock(A, 1, 5, ~~(Math.random() * lv) + 1);
+    drawBlockXY(A);
+    setBlock(B, 2, 5, ~~(Math.random() * lv) + 1);
+    drawBlockXY(B);
 }
 
 can.addEventListener('touchstart', function (e) {
